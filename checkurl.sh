011e01399ea438f2027f28ba1efb8ce7346bccb6
gwainjgp <juan.garrido@madrid.org>
2022-04-06 18:15:55 +0200

    Update checkurl.sh

--- checkurl.sh
+++ checkurl.sh
@@ -4,7 +4,7 @@ while read line
 do
   response=$(curl -I $line  2>/dev/null | head -n 1 | cut -d$' ' -f2 )
   result=$?
-  if [ "$?" -eq "0" ]; then
+  if [ "$result" -eq "0" ]; then
     echo "${line}|${response}"
   else
     echo "${line}|ERROR ${result}"
