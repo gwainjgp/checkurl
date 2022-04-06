7b799e3fd9e306c42c18f66dc04e381e921530f3
gwainjgp <juan.garrido@madrid.org>
2022-04-06 19:15:32 +0200

    Update checkurl.sh

--- checkurl.sh
+++ checkurl.sh
@@ -2,16 +2,20 @@
 
 while read line
 do
+  # Check por first . or * and chop it
+  if [[ $line =~ ^\. ]] || [[ $line =~ ^\* ]]; then
+    line="${line:1}"
+  fi
+  # Stop if asterisk
   if [[ $line =~ \* ]]; then
     echo "${line}| ERROR not URL"
     continue
   fi
-  if [[ $line =~ ^\. ]] || [[ $line =~ ^\* ]]; then
-    line="${line:1}"
-  fi
+  # Check DNS
   host=$(echo $line | cut -f1 -d/)
   host $host > /dev/null 2>&1
   result=$?
+  # Finally test URL if can be resolved
   if [ "$result" -eq "0" ]; then
     response=$(curl -I $line  2>/dev/null | head -n 1 | cut -d$' ' -f2 )
     if [ -z "$response" ]; then
