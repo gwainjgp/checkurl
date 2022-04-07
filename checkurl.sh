9edc575b49c579e1fe88b89fb8a618aac2d2b49a
jgp2585 <juan.garrido@madrid.org>
2022-04-07 10:59:32 +0200

    All Ok

--- checkurl.sh
+++ checkurl.sh
@@ -1,34 +1,52 @@
 #!/bin/bash
 
+#Functions
+checkWithCurl(){
+  response=$(curl -s -o /dev/null -I -w "%{http_code}" $1 )
+  if [ -z "$response" ]; then
+     echo "NOT host"
+  else
+    echo $response
+  fi
+}
+
 while read line
 do
   # Stop if asterisk not as first char
   if [[ $line =~ \* ]] && [[ ! $line =~ ^\*\. ]]; then
-    echo "${line}| ERROR not URL"
+    echo "${line}| Asterisk in middle| ERROR"
     continue
   fi
   original=$line
   # Check por first . or * and chop it
   # Detect if is a full URL
-  if [[ $line =~ ^\w+:// ]]; then
+  if [[ $line =~ [[:alnum:]]+:// ]]; then
     # Full URL
-    echo "${original}| Full URL"
-
+    response=$(checkWithCurl "$line"  )
+    echo "${original}| Full URL| ${response}"
   else
     # Other things
-    # Check domains
+    # Check partial domains
     if [[ $line =~ ^\. ]]; then
       line="${line:1}"
     fi
     if [[ $line =~ ^\*\. ]]; then
       line="${line:2}"
     fi
-    # Check URL
-    response=$(curl -s -o /dev/null -I -w "%{http_code}" $line )
-    if [ -z "$response" ]; then
-      echo "${original}|NOT host"
+    # Check if only domain
+    if [[ $line =~ \/ ]]; then
+      # Check URL
+      response=$(checkWithCurl "$line"  )
+      if [[ $response -eq "200" ]]; then
+        echo "${original}| Partial URL| ${response}"
+      else
+        response=$(checkWithCurl "https://${line}"  )
+        echo "${original}| Partial URL (https)| ${response}"
+      fi
     else
-      echo "${original}|${response}"
-    fi
+      # Check Domain or hostname
+      response=$(checkWithCurl "$line"  )
+      echo "${original}| Hostname| ${response}"
+   fi
   fi
 done < "${1:-/dev/stdin}"
