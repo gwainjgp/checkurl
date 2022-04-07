249c46489467e76aed33aa881b6dc325c5c2723d
jgp2585 <juan.garrido@madrid.org>
2022-04-07 09:48:19 +0200

    URL separation

--- checkurl.sh
+++ checkurl.sh
@@ -2,31 +2,33 @@
 
 while read line
 do
-  # Check por first . or * and chop it
-  if [[ $line =~ ^\. ]]; then
-    line="${line:1}"
-  fi
-  if [[ $line =~ ^\*\. ]]; then
-    line="${line:2}"
-  fi
-  # Stop if asterisk
-  if [[ $line =~ \* ]]; then
+  # Stop if asterisk not as first char
+  if [[ $line =~ \* ]] && [[ ! $line =~ ^\*\. ]]; then
     echo "${line}| ERROR not URL"
     continue
   fi
-  # Check DNS
-  host=$(echo $line | cut -f1 -d/)
-  host $host > /dev/null 2>&1
-  result=$?
-  # Finally test URL if can be resolved
-  if [ "$result" -eq "0" ]; then
-    response=$(curl -I $line  2>/dev/null | head -n 1 | cut -d$' ' -f2 )
-    if [ -z "$response" ]; then
-      echo "${line}|NOT host"
-    else
-      echo "${line}|${response}"
-    fi
+  original=$line
+  # Check por first . or * and chop it
+  # Detect if is a full URL
+  if [[ $line =~ ^\w+:// ]]; then
+    # Full URL
+    echo "${original}| Full URL"
+
   else
-    echo "${line}|ERROR DNS"
+    # Other things
+    # Check domains
+    if [[ $line =~ ^\. ]]; then
+      line="${line:1}"
+    fi
+    if [[ $line =~ ^\*\. ]]; then
+      line="${line:2}"
+    fi
+    # Check URL
+    response=$(curl -s -o /dev/null -I -w "%{http_code}" $line )
+    if [ -z "$response" ]; then
+      echo "${original}|NOT host"
+    else
+      echo "${original}|${response}"
+    fi
   fi
 done < "${1:-/dev/stdin}"
